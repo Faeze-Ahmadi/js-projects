2e4118be90a574a63a19c2f4e138027db4dc492d
Faeze <faezeahmadi7173@gmail.com>
2023-07-23 12:53:31 +0330

    feat/faeze/js-03/calculator-code

---
+++ js-03/index-03.js
@@ -0,0 +1,23 @@
+function calculator (a = 0, op, b = 0) {
+    if (!isNaN(a) && !isNaN(b)){
+        switch (op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return "operator is wrong!"
+        }
+    }
+    return "sth is wrong!"
+}
+
+const leftNum = +prompt("Enter Left Number:");
+const operator = prompt("Enter operator:");
+const rightNum = +prompt("Enter right Number:");
+
+console.log(calculator(leftNum, operator, rightNum));
