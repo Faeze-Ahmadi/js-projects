c3edac0da5d7f7720e3e9bb67328298cbb46a60d
Faeze <faezeahmadi7173@gmail.com>
2023-07-23 13:47:36 +0330

    debug/faeze/js-03/question-for-3-episode

---
+++ js-03/index-03.js
@@ -0,0 +1,29 @@
+const question = prompt("what codes do you want? calculator, capital letters or score?")
+
+
+
+if (question === "calculator") {
+    function calculator (a = 0, op, b = 0) {
+        if (!isNaN(a) && !isNaN(b)){
+            switch (op) {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    return a / b;
+                default:
+                    return "operator is wrong!"
+            }
+        } 
+        return "one of the numbers is not true!"
+    }
+    
+    const leftNum = +prompt("Enter Left Number:");
+    const operator = prompt("Enter operator:");
+    const rightNum = +prompt("Enter right Number:");
+    
+    console.log(calculator(leftNum, operator, rightNum));
+}
