bd156b22ef2e190b01ed5422c89a9237dd943475
Faeze <faezeahmadi7173@gmail.com>
2023-07-27 17:42:41 +0330

    feat/faeze/js-04/temperature-conversion

---
+++ js-04/index-04.js
@@ -0,0 +1,16 @@
+const question = prompt("temperature conversion, word search or product information?")
+
+if (question === "temperature conversion") {
+
+    function celsiusToFahrenheit(celsius) {
+        let fahrenheit = (celsius * 1.8) + 32;
+        return fahrenheit;
+      }
+      
+let celsius = parseFloat(prompt("Enter the degree of celsius to change in fahrenheit:"))
+console.log(celsiusToFahrenheit(celsius));
+
+
+
+} 
+  
