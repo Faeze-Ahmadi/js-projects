672bf7b53d1932392b082a820915b1363fc60343
Faeze <faezeahmadi7173@gmail.com>
2023-07-31 00:10:28 +0330

    debug/faeze/js-05/delete-if-function-obj-in-list

--- js-05/index-05.js
+++ js-05/index-05.js
@@ -1,20 +1,17 @@
-const randomList = [{
-    randomNumber,
-    randomDate,
-    randomWord,
-}]
+const randomList = []
 
+// values for randomDate
 const minDate = new Date('1970-01-01').getTime();
 const maxDate = new Date('2030-12-31').getTime();
-// const randomDate = new Date(Math.floor(Math.random() * (maxDate - minDate + 1) + minDate));
+
+// value for randomWord 
 const words = ['JavaScript', 'Python', 'C++', 'html', 'css', 'figma'];
-// const randomWord = words[Math.floor(Math.random() * words.length)];
 
 
-const addRandomParameter = (number, date, word) => {
-    if (!number && !date && !word) return null;
+
+const addRandomParameter = () => {
     const RandomParameter = {
-        randomNumber: Math.floor(Math.random() * (10000 - 1 + 1) + min),
+        randomNumber: Math.floor(Math.random() * (10000 - 1 + 1) + 1),
         randomDate: new Date(Math.floor(Math.random() * (maxDate - minDate + 1) + minDate)),
         randomWord: words[Math.floor(Math.random() * words.length)]
     }
