7cadb83f26fa87144595a46d59703fa439003439
Faeze <faezeahmadi7173@gmail.com>
2023-08-09 14:22:37 +0330

    feat/faeze/js-06/questionOF-add-remove-edit

--- js-06/index-06.js
+++ js-06/index-06.js
@@ -1,3 +1,5 @@
+const question = prompt("tell me what codes do you want: add, remove or edit object?");
+
 const persons = [{
     name: 'dorsa',
     age: 20,
@@ -15,20 +17,27 @@ const persons = [{
     field: 'economy'
 }]
 
-// const addNewPerson = (name, age, id, field) => {
-//     const name_ = prompt("tell the name?")
-//     const age_ = +prompt("tell the age?")
-//     const id_ = +prompt("tell the id?")
-//     const field_ = prompt("tell the field?")
-//     return {name_, age_, id_, field_}
 
-// }
-// persons.push(addNewPerson())
-// console.log(persons)
+if (question === "add") {
+const addNewPerson = (name, age, id, field) => {
+    const name_ = prompt("tell the name?")
+    const age_ = +prompt("tell the age?")
+    const id_ = +prompt("tell the id?")
+    const field_ = prompt("tell the field?")
+    return {name_, age_, id_, field_}
+}
+persons.push(addNewPerson())
+console.log(persons)
+
+
+ 
+} else if (question === "remove") {
+const indexNum = +prompt("tell the number of obj, that will remove?")
+persons.splice(indexNum, 1)
+console.log(persons)
 
 
 
-// const indexNum = +prompt("tell the number of obj, that will remove?")
-// persons.splice(indexNum, 1)
-// console.log(persons)
-
+} else if (question === "edit") {
+    
+}
