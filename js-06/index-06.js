882f5174519f5ade96c4820e155a3d6b7377e9bd
Faeze <faezeahmadi7173@gmail.com>
2023-08-09 13:16:48 +0330

    feat/faeze/js-06/list-of-persons-info

---
+++ js-06/index-06.js
@@ -0,0 +1,16 @@
+const persons = [{
+    name: 'dorsa',
+    age: 20,
+    id: 347,
+    field: 'mechanic'
+}, {
+    name: 'sahar',
+    age: 23,
+    id: 717,
+    field: 'camputer science'
+}, {
+    name: 'bahare',
+    age: 20,
+    id: 371,
+    field: 'economy'
+}]
