5673a3f91752b3e546f411fd743058abb8979600
Faeze <faezeahmadi7173@gmail.com>
2023-08-13 00:13:21 +0330

    faet/faeze/js-07/list-of-persons

--- js-07/index-07.ts
+++ js-07/index-07.ts
@@ -11,3 +11,40 @@ type persons = {
     phone_number: number;
 }
 
+const persons_info: Array<persons> = [
+    {
+        name: "faeze",
+        family_name: "ahamdi",
+        age: 20,
+        job: "writer",
+        married: false,
+        genger: "female",
+        height: 157,
+        weight: 48,
+        id: 717,
+        phone_number: 9196798193,
+    }, {
+        name: "sara",
+        family_name: "moradi",
+        age: 25,
+        job: "teacher",
+        married: true,
+        genger: "female",
+        height: 167,
+        weight: 56,
+        id: 219,
+        phone_number: 9177568909,
+    }, {
+        name: "matin",
+        family_name: "rad",
+        age: 30,
+        job: "bloger",
+        married: true,
+        genger: "male",
+        height: 191,
+        weight: 87,
+        id: 313,
+        phone_number: 9142498734,
+    }
+]
+
