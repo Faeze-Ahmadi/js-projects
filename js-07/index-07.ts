2ec711ef43bd4873c181dc7ead856c0a4462180c
Faeze <faezeahmadi7173@gmail.com>
2023-08-13 00:12:41 +0330

    faet/faeze/js-07/type-of-persons

--- js-07/index-07.ts
+++ js-07/index-07.ts
@@ -1,1 +1,13 @@
-console.log("faeze");
+type persons = {
+    name: string;
+    family_name: string;
+    age: number;
+    job: string;
+    married: boolean;
+    genger: "male" | "female";
+    height: number | string;
+    weight: number | string;
+    id: number;
+    phone_number: number;
+}
+
